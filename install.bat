b77e587319f76b3e4b5d09b0fae8e251f044ddfe
tom <tom@sapletta.com>
2020-04-29 12:27:10 +0200

    update info

--- install.bat
+++ install.bat
@@ -1,11 +1,9 @@
 :: devops.bat install php
--curl.bat https://www.python.org/ftp/python/3.7.4/python-3.7.4-amd64.exe python-3.7.4-amd64.exe
-::-curl.bat https://www.python.org/ftp/python/3.7.4/python-3.7.4-amd64.exe
-::python-3.7.4-amd64.exe /quiet
+-curl.bat https://www.python.org/ftp/python/3.8.2/python-3.8.2-amd64.exe python-3.8.2-amd64.exe
 :: https://docs.python.org/3/using/windows.html#the-full-installer
-python-3.7.4-amd64.exe /quiet InstallAllUsers=1 PrependPath=1 Include_test=0 DefaultAllUsersTargetDir=C:\python374
-::msiexec /i python-3.7.4-amd64.exe TARGETDIR=C:\python374
-set PATH=C:\python374;%PATH%
-set PYTHONPATH=%PYTHONPATH%;C:\python374
+python-3.8.2-amd64.exe /quiet InstallAllUsers=1 PrependPath=1 Include_test=0 DefaultAllUsersTargetDir=C:\python374
+::msiexec /i python-3.8.2-amd64.exe TARGETDIR=C:\python374
+set PATH=C:\python382;%PATH%
+set PYTHONPATH=%PYTHONPATH%;C:\python382
 ::echo %PATH%
 py -m pip install -r src/requirements.txt
