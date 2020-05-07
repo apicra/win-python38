8b8db08428287769ff461401bc1b7cd48554d0db
tom <tom@sapletta.com>
2020-05-07 17:14:46 +0200

    update py

--- install.sh
+++ install.sh
@@ -3,5 +3,5 @@ apt install python3-venv -y
 apt install gunicorn -y
 python3 -m pip install --user virtualenv
 python3 -m venv env
-source env/bin/activate
+. env/bin/activate
 python3 -m pip install -r src/requirements.txt
